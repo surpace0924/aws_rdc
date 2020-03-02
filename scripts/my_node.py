97d6ca0087e3f98f61a9ee016da11a28fc012b26
ryoga <surpace2479@gmail.com>
2020-03-02 12:31:44 +0900

    軌道追従の実装

--- scripts/my_node.py
+++ scripts/my_node.py
@@ -16,20 +16,6 @@ from geometry_msgs.msg import Vector3
 from visualization_msgs.msg import Marker
 import numpy as np
 
-class Trajectory:
-    def __init__(self, x, y):
-        tmp_num = 0
-        tmp_pos = list(range(0))
-        for i in range(len(x)):
-            tmp_pos.append([x[i], y[i]])
-            tmp_num += 1
-
-        self.pos = np.array(tmp_pos)
-        self.num = tmp_num
-        self.id = None
-        self.selected_id = 0;
-
-
 def euler_to_quaternion(euler):
     q = tf.transformations.quaternion_from_euler(euler.x, euler.y, euler.z)
     return Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
@@ -41,6 +27,25 @@ def callback(vel):
     # rospy.loginfo("now: %f", now.to_sec())
 
 
+class PurePursuitControl:
+    def __init__(self, trajectory):
+            self.trajectory = trajectory
+
+    def getLiner(self, pose, ind):
+        tx = self.trajectory[ind][0]
+        ty = self.trajectory[ind][1]
+        L = math.sqrt((tx - pose[0])**2 + (ty - pose[1])**2)
+        Kp = 1
+        liner = Kp * L
+        return liner
+
+    def getOmega(self, pose, ind):
+        tx = self.trajectory[ind][0]
+        ty = self.trajectory[ind][1]
+
+        error_angle = math.atan2(ty - pose[1], tx - pose[0]) - pose[2]
+        return error_angle * 5
+
 
 if __name__ == '__main__':
     rospy.init_node('my_node')
@@ -50,10 +55,10 @@ if __name__ == '__main__':
     pub_terget_pos = rospy.Publisher('terget_pos', PoseStamped, queue_size=100)
     listener = tf.TransformListener()
 
-    t = np.arange(0, 3.14*2, 0.1)
-    cx = [3 * math.cos(ix) for ix in t]
-    cy = [2 * math.sin(ix) for ix in t]
-    trajectory = Trajectory(cx,cy)
+    # t = np.arange(0, 3.14*2, 0.1)
+    # cx = [3 * math.cos(ix) for ix in t]
+    # cy = [2 * math.sin(ix) for ix in t]
+    # trajectory = Trajectory(cx,cy)
 
     file_name = "/home/ryoga/catkin_ws/src/aws_rdc/scripts/route.csv"
     p2 = np.genfromtxt(file_name, delimiter=',', filling_values = 0)
@@ -65,6 +70,8 @@ if __name__ == '__main__':
         pose.position.y = p2[i][1]
         points.poses.append(pose)
 
+    ppc = PurePursuitControl(p2)
+
     count = 0
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
@@ -73,12 +80,7 @@ if __name__ == '__main__':
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
 
-        twist = Twist()
-        twist.linear.x = 1.5
-        twist.angular.z = -1.9 * angular[2]
-        if linear[0] > 4.2:
-            twist.angular.z += 2
-        pub_vel.publish(twist)
+
 
         points.header.frame_id = 'map'
         points.header.stamp = rospy.Time.now()
@@ -94,9 +96,23 @@ if __name__ == '__main__':
             terget_pos.pose.position.x = p2[len(p2)/2][0]
             terget_pos.pose.position.y = p2[len(p2)/2][1]
 
+
         points.poses.append(pose)
         pub_terget_pos.publish(terget_pos)
 
+        print(linear)
+        now_pose = [0,0,0]
+        now_pose[0] = linear[0]
+        now_pose[1] = linear[1]
+        now_pose[2] = angular[2]
+        twist = Twist()
+        if count < len(p2)/2:
+            twist.linear.x = ppc.getLiner(now_pose, count)
+            twist.angular.z = ppc.getOmega(now_pose, count)
+        else:
+            twist.linear.x = ppc.getLiner(now_pose, len(p2)/2)
+            twist.angular.z = ppc.getOmega(now_pose, len(p2)/2)
+        pub_vel.publish(twist)
 
 
         # rospy.loginfo("pose [%.2f %.2f %.2f]", linear[0], linear[1], linear[2])
