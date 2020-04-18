0ab8a4ce6e6f03e0d9e7768aa04579728f997199
ryoga <surpace2479@gmail.com>
2020-04-18 11:23:53 +0900

    update: Topic通信のテスト

--- src/aws_rdc_pre_node.cpp
+++ src/aws_rdc_pre_node.cpp
@@ -1,10 +1,35 @@
 #include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
 #include "../../MyStdLib/MyStdLib.h"
+
+// 機体性能最高速度
+constexpr double MAX_SPD_LINEAR = 0.225; // [m/s]
+constexpr double MAX_SPD_ANGULAR = 2.84; // [rad/s]
+
+// 制御周期
+constexpr double LOOP_HZ = 50.0;
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "aws_rdc_pre_node");
-    ROS_INFO("HELLO");
-    std::cout << Vector2::getAngle(Vector2(0, 0), Vector2(1, 1.73205)) * RAD_TO_DEG << std::endl;
+    ros::NodeHandle nh;
+    ros::Rate rate(LOOP_HZ);
+    geometry_msgs::Twist vel;
+
+    ros::Publisher pub_cmd_vel;
+    pub_cmd_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    vel.linear.x = 0.5;
+    vel.angular.z = 1.0;
+
+    while (ros::ok())
+    {
+        ROS_INFO("HELLO");
+
+        pub_cmd_vel.publish(vel);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
     return 0;
 }
